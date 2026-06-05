37462f7ef5150f7437d0370fe9df8601d82177fa
AbdallaZaid <abdallazeed3@gmail.com>
2026-06-05 04:32:29 +0300

    chore: remove everything outside oldP folder

--- logic/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  TIMER_DURATION: 20,
-  HISTORY_KEY: 'quizHistory',
-  STATE_KEY: 'quizState',
-  TOKEN_KEY: 'opentdb_token',
-  API_BASE: 'https://opentdb.com/api.php',
-  API_DEFAULT: 'amount=10&category=18&difficulty=easy&type=multiple',
-}
--- logic/state.js
+++
@@ -1,9 +0,0 @@
-const QuizApp = {
-  quizQuestions: [],
-  currentQuestionIndex: 0,
-  score: 0,
-  userAnswers: [],
-  isApiMode: false,
-  timerRemaining: CONFIG.TIMER_DURATION,
-  timerInterval: null,
-}
